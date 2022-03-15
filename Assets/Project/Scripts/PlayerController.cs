7e50697d48821f2e507dcf05ea9550e0ae0b3bc8
Shofnip <shofnip@gmail.com>
2022-03-15 04:55:06 -0300

    Added Jump Action with Animation

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -18,6 +18,12 @@ public class PlayerController : MonoBehaviour
 
   private bool facingRight = true;
 
+  private bool isJumpActive = false;
+  private int numbersOfJump = 0;
+  private int limitOfJumps = 2;
+  [SerializeField]
+  private float jumpForce;
+
   // Start is called before the first frame update
   void Start()
   {
@@ -28,24 +34,50 @@ public class PlayerController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    isGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+    playerAnimator.SetBool("isGrounded", isGround);
+
     touchRun = Input.GetAxisRaw("Horizontal");
+    SetMovementAnimation();
+
+    if(Input.GetButtonDown("Jump")) {
+      isJumpActive = true;
+    }
   }
 
   void FixedUpdate()
   {
     MovePlayer(touchRun);
+    if(isJumpActive) JumpPlayer();
   }
 
-  void MovePlayer(float horizontalMovement) {
+  void MovePlayer(float horizontalMovement)
+  {
     playerRigidbody2D.velocity = new Vector2(touchRun * speed, playerRigidbody2D.velocity.y);
 
-    if (touchRun > 0 && !facingRight || touchRun < 0 && facingRight) {
+    if (touchRun > 0 && !facingRight || touchRun < 0 && facingRight)
+    {
       FlipPlayerSide();
     }
   }
 
-  void FlipPlayerSide () {
+  void JumpPlayer() {
+    if (isGround) {
+      playerRigidbody2D.AddForce(new Vector2(0, jumpForce));
+      isGround = false;
+    }
+    isJumpActive = false;
+  }
+
+  void FlipPlayerSide()
+  {
     facingRight = !facingRight;
     transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+  }
+
+  void SetMovementAnimation()
+  {
+    playerAnimator.SetBool("isWalking", playerRigidbody2D.velocity.x != 0 && isGround);
+    playerAnimator.SetBool("isJumping", !isGround);
   }
 }
