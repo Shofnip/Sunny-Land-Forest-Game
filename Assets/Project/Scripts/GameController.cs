b9bf5898edc65bcea9f66762fadc4e6c19ac7b3e
Shofnip <shofnip@gmail.com>
2022-03-16 12:22:19 -0300

    Add BGM and Collect Carrot Audio

--- Assets/Project/Scripts/GameController.cs
+++ Assets/Project/Scripts/GameController.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     private int score;
-    [SerializeField]
-    private Text txtScore;
+    [SerializeField] private Text txtScore;
+
+    [SerializeField] private AudioSource fxGame;
+    [SerializeField] private AudioClip fxCollectCarrot;
 
     public void AddScore (int qntToAdd) {
         score += qntToAdd;
         txtScore.text = score.ToString();
+        fxGame.PlayOneShot(fxCollectCarrot);
     }
 }
